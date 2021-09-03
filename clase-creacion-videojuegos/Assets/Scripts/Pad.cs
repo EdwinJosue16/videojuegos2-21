397d55a1c8aa15137404d604414a148a585af679
Jose Antonio Ciccio <jciccio@gmail.com>
2021-09-03 07:36:07 -0600

    Texturas y Scripts agregados para físicas

---
+++ clase-creacion-videojuegos/Assets/Scripts/Pad.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pad : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] float screenSizeUnits = 17.76f;
+    [SerializeField] float minX = 0;
+    [SerializeField] float maxX = 16.76f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float paddlePos = Input.mousePosition.x/Screen.width * screenSizeUnits;
+        //transform.position = new Vector2(paddlePos, transform.position.y);
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        position.x = Mathf.Clamp(paddlePos, minX, maxX);
+        transform.position = position;
+    }
+}
---
+++ clase-creacion-videojuegos/Assets/Scripts/Prueba.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("En El Método Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("En El Método Update");
+    }
+}
