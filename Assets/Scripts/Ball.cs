d631877000fb513e4d9364db3b2ad4e4bd61ad69
ebrenes-dev <ebrenes@soin.co.cr>
2021-09-10 02:06:05 -0600

    Tarea finalizada

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,35 +8,68 @@ public class Ball : MonoBehaviour
     // Bandera
     // Tener la referencia a la paleta
     [SerializeField] Pad paddle;
-    [SerializeField] Vector2 velocity = new Vector2(1f,4f);
+    [SerializeField] Vector2 velocity = new Vector2(1f, 4f);
     bool playing = false;
-    Vector2 paddleToBallDistance; 
-    // Start is called before the first frame update
+    Vector2 paddleToBallDistance;
+
+    Rigidbody2D ball;
+
     void Start()
     {
+        ball = GetComponent<Rigidbody2D>();
         paddleToBallDistance = transform.position - paddle.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         LockToPaddle();
         LaunchOnClick();
     }
 
-    void LaunchOnClick(){
-        if(Input.GetMouseButtonDown(0)){
+    void LaunchOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             playing = true;
-            GetComponent<Rigidbody2D>().velocity = velocity;
+            ball.velocity = velocity;
         }
     }
 
-    void LockToPaddle(){
-        if(!playing){
+    void LockToPaddle()
+    {
+        if (!playing)
+        {
             Vector2 paddleRef = paddle.transform.position;
-            Vector2 paddlePos = new Vector2(paddleRef.x,paddleRef.y);
+            Vector2 paddlePos = new Vector2(paddleRef.x, paddleRef.y);
             transform.position = paddlePos + paddleToBallDistance;
         }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        var contactPoint = other.contacts[0];
+        Bounce(contactPoint);
+    }
+    void Bounce(ContactPoint2D contact)
+    {
+        // Caso de la pared izquierda: 
+        /*
+            |    /  : reflection
+            |   /
+            |  /
+            | /
+            |---------- : normal
+            | \
+            |  \
+            |   \
+            |    \  : ballVelocity
+        */
+        // Los otros son analogos
+
+        Vector2 ballVelocity = ball.velocity;
+        Vector2 normal = contact.normal;
+        Debug.Log(normal);
+        Vector2 reflection = Vector2.Reflect(ballVelocity.normalized, normal);
+        ball.velocity = reflection * velocity.magnitude;
     }
 
 }
